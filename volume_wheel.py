29e10ea85494543dcdb2640df2588ae5e38d3ed0
JF-2121 <joshuafridel24@gmail.com>
2025-06-29 18:23:03 +0200

    final touches <BS>O

--- volume_wheel.py
+++ volume_wheel.py
@@ -1,18 +1,25 @@
+#!/usr/bin/env python3
 from evdev import InputDevice, ecodes
 import pulsectl
 
-dev = InputDevice('/dev/input/event3')  # dein Mausrad-Gerät anpassen
+dev = InputDevice('/dev/input/event3')  # Anpassen auf dein Gerät
+
 pulse = pulsectl.Pulse('volume-wheel')
 
+def change_volume(delta):
+    default_sink_name = pulse.server_info().default_sink_name
+    sink = pulse.get_sink_by_name(default_sink_name)
+    vol = sink.volume
+    new_values = [min(max(v + delta, 0.0), 1.0) for v in vol.values]
+    new_vol = pulsectl.PulseVolumeInfo(new_values)
+    pulse.volume_set(sink, new_vol)
+
 for event in dev.read_loop():
-    if event.type == ecodes.EV_REL:
-        if event.code == ecodes.REL_HWHEEL or event.code == ecodes.REL_WHEEL:
-            if event.value > 0:
-                print("Volume up")
-                pulse.volume_change_all_chans(0.05)
-            elif event.value < 0:
-                print("Volume down")
-                pulse.volume_change_all_chans(-0.05)
-        else:
-            # Andere Events ignorieren, z.B. Maustasten
-            pass
+    if event.type == ecodes.EV_REL and event.code == ecodes.REL_HWHEEL:
+        if event.value < 0:
+            print("Volume up")
+            change_volume(0.05)
+        elif event.value > 0:
+            print("Volume down")
+            change_volume(-0.05)
+    # Alles andere ignorieren, also auch REL_WHEEL (normales Scrollrad)
